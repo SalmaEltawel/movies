64cde708746660b60eb44f4326b6e271ba75d177
motaheda <Nadahamdy171999@gmail.com>
2024-04-17 03:38:52 +0200

    add carousel_slider

--- lib/feature/home/presentation/pages/tabs/home_tab.dart
+++ lib/feature/home/presentation/pages/tabs/home_tab.dart
@@ -7,7 +7,7 @@ import 'package:movies/feature/home/data/models/popular_model.dart';
 import 'package:movies/feature/home/presentation/bloc/home_bloc.dart';
 import 'package:movies/feature/home/presentation/pages/items/popular_item.dart';
 import 'package:movies/feature/home/presentation/pages/items/up_coming_item.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import '../items/top_rated_item.dart';
 
 class HomeTab extends StatelessWidget {
@@ -29,67 +29,84 @@ class HomeTab extends StatelessWidget {
         //         title: Center(child: CircularProgressIndicator())),
         //   );
 
-         if (state.getPopularStatus == RequestStatus.failures ||
+        if (state.getPopularStatus == RequestStatus.failures ||
             state.getTopRatedStatus == RequestStatus.failures ||
-            state.getUpComingStatus == RequestStatus.failures
-        ) {
+            state.getUpComingStatus == RequestStatus.failures) {
           Center(child: Text("error"));
         }
       },
-
       builder: (context, state) {
         return ScreenUtilInit(
-            builder: (context, widget) {
-              ScreenUtil.init(
-                  context, designSize: Size(412, 892), splitScreenMode: true,minTextAdapt: true,
-              );
-              return
-                Scaffold(
-                  backgroundColor: Colors.black,
-                  body: Column(
-                    children: [
+          builder: (context, widget) {
+            ScreenUtil.init(
+              context,
+              designSize: Size(412, 892),
+              splitScreenMode: true,
+              minTextAdapt: true,
+            );
+            return Scaffold(
+              backgroundColor: Colors.black,
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    CarouselSlider(
+                      items: [
+                        Padding(
+                          padding: const EdgeInsets.all(25.0),
+                          child: state.popularModel != null
+                              ? SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,physics: NeverScrollableScrollPhysics(),
+                                  child: PopularItem(
+                                      state.popularModel?.results ?? []))
+                              : Center(child: CircularProgressIndicator()),
+                        ),
+                      ],
+                      options: CarouselOptions(
+                        height: 200.h,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                        Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 10),
+                            color: AppColors.backGround,
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(("New Releases"),
+                                      style: TextStyle(color: AppColors.white)),
+                                  state.upComingModel != null
+                                      ? UpComingItem(
+                                          state.upComingModel?.results ?? [])
+                                      : Center(
+                                          child: CircularProgressIndicator()),
+                                ])),
 
-                      state.popularModel != null
-                          ? PopularItem(state.popularModel?.results ?? [])
-                          : Center(child: CircularProgressIndicator()),
-                      SizedBox(height: 20.h,),
-                      Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 10),
-                          color: AppColors.backGround,
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(("New Releases"), style: TextStyle(
-                                    color: AppColors.white)),
-                                state.upComingModel != null
-                                    ? UpComingItem(state.upComingModel
-                                    ?.results ?? [])
-                                    : Center(
-                                    child: CircularProgressIndicator()),
-                              ])),
-                      SizedBox(height: 20.h,),
-                      Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 10),
-                          color: AppColors.backGround,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(("Recommended"), style: TextStyle(
-                                  color: AppColors.white)),
-                              state.topRatedModel != null
-                                  ? TopRatedItem(state.topRatedModel?.results ??
-                                  [])
-                                  : Center(child: CircularProgressIndicator()),
-                            ],
-                          )
-                      )
-
-                    ],
-                  ),
-                );
-            },
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        color: AppColors.backGround,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(("Recommended"),
+                                style: TextStyle(color: AppColors.white)),
+                            state.topRatedModel != null
+                                ? TopRatedItem(
+                                    state.topRatedModel?.results ?? [])
+                                : Center(child: CircularProgressIndicator()),
+                          ],
+                        ))
+                  ],
+                ),
+              ),
+            );
+          },
         );
       },
     );
